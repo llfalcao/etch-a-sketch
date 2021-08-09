a0fc4a8c5a7e383e038e0dd541092517f0a64b5b
llfalcao <luizotavio-falcao@hotmail.com>
2021-08-08 23:45:41 -0300

    Enable resizable canvas

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const body = document.querySelector('body');
-console.log(body);
-
+const clearBtn = document.querySelector('#clear');
 const divContainer = document.createElement('div');
-divContainer.classList.add('container');
-body.appendChild(divContainer);
 
-for (let i = 1; i <= 256; i++) {
-    const div = document.createElement('div');
-    div.id = `square-${i}`;
-    div.classList.add('square');
-    divContainer.appendChild(div);
+function removeGrid() {
+    const divContainer = document.querySelector('.container');
+    divContainer.remove();
 }
+
+function createGrid(size) {
+    lastKnownSize = size;
+    const divContainer = document.createElement('div');
+
+    divContainer.classList.add('container');
+    divContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    divContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    body.appendChild(divContainer);
+
+    const squaredSize = size * size;
+
+    for (let i = 1; i <= squaredSize; i++) {
+        const div = document.createElement('div');
+        div.id = `square-${i}`;
+        div.classList.add('square');
+        divContainer.appendChild(div);
+    }
+}
+
+clearBtn.addEventListener('click', (e) => {
+    const newGridSize = window.prompt(
+        'Please type the number of squares per side of the new canvas:'
+    );
+    removeGrid();
+    createGrid(newGridSize);
+});
+
+let lastKnownSize;
+createGrid(16);
